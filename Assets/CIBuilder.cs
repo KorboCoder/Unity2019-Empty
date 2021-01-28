86971acac92b7d0b8389fc07614028693150bba4
KorboCoder <daniel.v@anino.co>
2021-01-28 21:54:10 +0800

    added build scripts

---
+++ Assets/CIBuilder.cs
@@ -0,0 +1,9 @@
+﻿using UnityEditor;
+class CIBuilder
+{
+    static void PerformBuild()
+    {
+        string[] scenes = { "Assets/SampleScene.unity" };
+        BuildPipeline.BuildPlayer(scenes, "/root/bin", BuildTarget.Android, BuildOptions.None);
+    }
+}
